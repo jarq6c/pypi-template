e1c3bc34ed0b5d14e5abca7fc2d6f46a68e55c9a
Jason Regina <jarq6c@gmail.com>
2023-02-14 07:53:07 -0600

    update package defaults

--- tests/test_hello.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from hello.hello import greet
-
-def test_greet():
-    # Create a greeting
-    greeting = greet("Jo")
-
-    # Check the greeting
-    assert greeting == "Hello, Jo!"
